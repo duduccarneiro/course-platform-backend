4f9d17f82c9f04bff8f13723a4d557bbc6a3c13b
Eduardo Carneiro <duduccarneiro@gmail.com>
2024-07-30 18:20:18 -0300

    Me endpoint added to retrieve authenticated user data

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\MeController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\RegisterUserController;
 use Illuminate\Http\Request;
@@ -14,4 +15,8 @@ Route::group(['prefix' => 'auth'], function() {
     Route::post('/register', RegisterUserController::class);
 
     Route::post('/token', [LoginController::class, 'store']);
+
+    Route::middleware('auth:sanctum')->group(function() {
+        Route::get('/me', [MeController::class, '__invoke']);
+    });
 });
